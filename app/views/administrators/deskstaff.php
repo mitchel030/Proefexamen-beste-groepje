279108d756490be9cda184c22d43fe65a54e91a6
zStevenn <327068@student.mboutrecht.nl>
2022-04-20 14:20:03 +0200

    Created table on admin/deskstaff.php

---
+++ app/views/administrators/deskstaff.php
@@ -0,0 +1,37 @@
+<!-- Require head.php -->
+<?php require_once APPROOT . '\views\includes\head.php'; ?>
+
+<a href="<?= URLROOT ?>/administrators/index">Back to admin dashboard</a></li>
+
+<div class="container">
+  <div class="row">
+    <div class="col-10">
+      <h1>Student Overview</h1>
+      <a class="btn btn-primary" href="<?= URLROOT ?>/administrators/studentadd" role="button">Create a new user</a>
+      <!-- Table with student information -->
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <!-- Table column names -->
+            <th scope="col">Studentnumber</th>
+            <th scope="col">Firstname</th>
+            <th scope="col">Infix</th>
+            <th scope="col">Lastname</th>
+            <th scope="col">Email</th>
+            <th scope="col">Class</th>
+            <th scope="col">Edit student</th>
+            <th scope="col">Delete student</th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- Echo table `student` data concatenated into HTML in $data -->
+          <?= $data["overview"] ?>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+
+<!-- Require end.php -->
+<?php require_once APPROOT . '\views\includes\end.php'; ?>

223af4700f92375ef0e8e756a2bd71ae50daf584
mitchel030 <317939@student.mboutrecht.nl>
2022-04-20 14:01:39 +0200

    administrator controller

--- app/controllers/Administrators.php
+++ app/controllers/Administrators.php
@@ -6,47 +6,85 @@ class Administrators extends Controller
     $this->adminModel = $this->model('Administrator');
   }
 
-  
-
   public function index()
-  {
-    $this->view('administrators/index');
-  
+	{		
 		// Retrieve all data from assortment table
-		$leraar = $this->adminModel->getLeraar();
+		$item = $this->adminModel->getItems();
 		
 		// Create HTML Rows using assortment data
-		$leraarRows = "";
-		foreach ($leraar as $a) {
-			$leraarRows .= "<tr>";
-			$leraarRows .= "<th scope='row'>".$a->id."</th>";
-			$leraarRows .= "<td>".$a->afkorting."</td>";
-			$leraarRows .= "<td>".$a->naam."</td>";
-			$leraarRows .= "<td>".$a->achternaam."</td>";
-			$leraarRows .= "<td>".$a->email."</td>";
-      $leraarRows .= "<td>".$a->telefoon."</td>";
-			$leraarRows .= "<td>".$a->klas."</td>";
-			$leraarRows .= "<td>
-														<a class='btn btn-xs btn-info' href=/warehouseadmins/edit?id=$a->id>Edit
+		$itemRows = "";
+		foreach ($item as $a) {
+			$itemRows .= "<tr>";
+			$itemRows .= "<th scope='row'>".$a->id."</th>";
+			$itemRows .= "<td>".$a->afkorting."</td>";
+			$itemRows .= "<td>".$a->naam."</td>";
+			$itemRows .= "<td>".$a->achternaam."</td>";
+			$itemRows .= "<td>".$a->email."</td>";
+    		 $itemRows .= "<td>".$a->telefoon."</td>";
+			$itemRows .= "<td>".$a->klas."</td>";
+			$itemRows .= "<td>
+														<a class='btn btn-xs btn-info' href=/administrators/edit?id=$a->id>Edit
 													</td>";
-			$leraarRows.= "<td>
-														<a class='btn btn-xs btn-info' href=/warehouseadmins/destroy?id=$a->id>Delete
+			$itemRows.= "<td>
+														<a class='btn btn-xs btn-info' href=/administrators/destroy?id=$a->id>Delete
 													</td>";
-			$leraarRows .= "</tr>";
+			$itemRows .= "</tr>";
 		}
 
 		$this->view('administrators/index', $indexData = [
-			"leraar" => $leraarRows
+			"item" => $itemRows
 		]);
-  }
+	}
 
-  public function edit()
-  {
-    $this->view('administrator/edit');
-  }
+	//Redirects to create.php
+	public function create()
+	{
+		$this->view('administrators/create');
+	}
 
-  public function delete()
-  {
-    $this->view('administrator/delete');
-  }
+	//Stores the information in database through adminModel
+	public function store()
+	{
+		$afkorting  = $_POST['afkorting'];
+		$naam = $_POST['naam'];
+		$achternaam = $_POST['achternaam'];
+		$email = $_POST['email'];
+        $telefoon  = $_POST['telefoon'];
+		$klas = $_POST['klas'];
+	
+
+
+		$this->adminModel->store($afkorting, $naam, $achternaam, $email, $telefoon, $klas);
+		$this->redirect('administrators');
+	}
+
+	//Redirects to edit.php with the data from the corresponding ID
+	public function edit()
+	{
+		$data = $this->adminModel->edit();
+		$this->view('administrators/edit', $data);
+	}
+
+	//Updates the information in database through adminModel
+	public function update($id)
+	{
+		$afkorting  = $_POST['afkorting'];
+		$naam = $_POST['naam'];
+		$achternaam = $_POST['achternaam'];
+		$email = $_POST['email'];
+        $telefoon  = $_POST['telefoon'];
+		$klas = $_POST['klas'];
+
+		$this->adminModel->update($id, $afkorting, $naam, $achternaam, $email, $telefoon, $klas);
+		//Redirect to warehouseadmins view
+		$this->redirect('administrators');
+	}
+
+	//Delete through adminModel and redirect to warehouseadmins view
+	public function destroy()
+	{
+		$this->adminModel->destroy();
+
+		$this->redirect('administrators');
+	}
 }
